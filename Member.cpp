a030eca365271cf9346b962f159e9457355f3d74
yishay1 <31584757+yishay1@users.noreply.github.com>
2018-04-10 02:20:57 -0700

    add comments

--- Member.cpp
+++ Member.cpp
@@ -1,37 +1,38 @@
 #include "Member.h"
 int Member::cntr=0;
-
+/*constructor*/
 Member::Member(){
     id=++cntr;
 }
-
+/*add this member anothr member to following list
+and add to another member this to followers list  */
 void Member:: follow(Member &member)
 {
 	following[member.id]=&member;
 	member.followers[id]=this;
 }
-
+/*remove the following from 2 lists */
 void Member:: unfollow(Member &member)
 {
 	following.erase(member.id);
 	member.followers.erase(this->id);
 }
-
+/*return how many following after this member*/
 int Member::numFollowers()
 {
 	return followers.size();
 }
-
+/*return how many members this member following*/
 int Member::numFollowing()
 {
 	return following.size();
 }
-
+/* return how many users in this social network*/
 int Member::count()
 {
 	return cntr;
 }
-
+/*destructor- remove this member from all the lists of all the members*/
 Member::~Member()
 {
 	map <int, Member*> :: iterator i;
